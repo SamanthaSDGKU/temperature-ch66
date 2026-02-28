ed57a02af2cc468f6617244f821e75953a9bb622
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2026-02-28 11:24:16 -0800

    variables

---
+++ script.js
@@ -0,0 +1,68 @@
+console.log("-- Variable --");
+
+//String (text) "" 
+let firstName = "Samantha"; // assign the value
+// numerical variable 2
+let myAge = 99;
+//boolean (true / false)
+let isStudent = false;
+let isProfessor = true;
+//displaying the values
+console.log(firstName);
+console.log(myAge);
+console.log(isStudent);
+console.log(isProfessor);
+
+// Mini challenge:
+// Create a variable for your last name.
+// Create a variable for your height.
+// Finally, show each variable separately in the console.
+let lastName = "Rife";
+let phone = "667-090-4954"
+let gpa = 4.0;
+let ticketCost = 200.00;
+let myHeight= 6.1;
+
+let street = "Palm ave";
+let number = "123";
+let city = "Temecula";
+let zip = "22667";
+
+console.log(number,street,zip,city);
+
+console.log(lastName);
+console.log(zip);
+console.log(phone);
+console.log(myHeight + "ft"); // concatenation
+
+// Arithmetic operations
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+
+
+//console.log(num1 + " + " + num2 + " = " + sum);
+console.log(`${num1} + ${num2} = ${sum}`);
+console.log(`${num1} - ${num2} = ${sub}`);
+console.log(`${num1} x ${num2} = ${mult}`);
+console.log(`${num1} / ${num2} = ${div}`);
+
+// calculating the area of a circle
+let pi = 3.1416;
+let radius = 3;
+let area = pi * radius * radius;
+
+console.log(`The area of a circle with ${radius} is ${area}`);
+
+let aVariable = 40;
+aVariable = 50;
+aVariable = 60;
+
+console.log(aVariable);
+
+const weekDays = 7; // it is the same
+//weekDays = 9; - > ERROR 
